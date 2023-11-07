41e4863234c3693340ee3210b997f80679e8dfc6
lisaparida1 <146925253+lisaparida1@users.noreply.github.com>
2023-11-07 13:51:52 +0530

    List and Dictionary comprehension used along with Exception Handling

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -3,11 +3,19 @@ import pandas
 #  How to iterate through Dataframes:
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-word = input("Enter a word: ")
-list_word = list(word.upper())
-
 nato_phonetic = pandas.read_csv("nato_phonetic_alphabet.csv")
 dict_nato = {row.letter:row.code for (index, row) in nato_phonetic.iterrows()}
 
-list_nato_names = [dict_nato[letter] for letter in list_word if letter in dict_nato]
-print(list_nato_names)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        list_nato_names = [dict_nato[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters from alphabets allowed!")
+        generate_phonetic()
+    else:
+        print(list_nato_names)
+
+
+generate_phonetic()
